90a92b12ccc94716c61e76853dc98fa17587e130
Kauã Maximino <kaua_maximino@outlook.com>
2022-05-25 23:02:11 -0300

    projeto finalizado e arrumando indentação

--- desafio24.sql
+++ desafio24.sql
@@ -1,5 +1,8 @@
-UPDATE northwind.order_details
+UPDATE
+    northwind.order_details
 SET
     discount = 30
 WHERE
-    unit_price > 10.0000 AND id BETWEEN 30 AND 40;
+    unit_price > 10.0000
+    AND id BETWEEN 30
+    AND 40;
